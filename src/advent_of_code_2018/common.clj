5c2e9cb858049fd722dcec53825a0744dec7b529
Morgan Bentell <morgan.bentell@evry.com>
2018-12-01 13:03:07 +0100

    day 1 part 2

--- src/advent_of_code_2018/common.clj
+++ src/advent_of_code_2018/common.clj
@@ -7,3 +7,17 @@
             (is (= (clean-input "5\n-5") [5 -5])))}
   [input-str]
   (map read-string (string/split input-str #"\n")))
+
+(defn circular-nth
+  {:test (fn []
+           (is (= (circular-nth [1 2] 3) 1))
+           (is (= (circular-nth [1] 1) 1))
+           (is (= (circular-nth [1] 2) 1))
+           (is (= (circular-nth [1 2 3 4 5] 7) 2)))}
+  [col n]
+  (nth col (mod (dec n) (count col))))
+
+(defn printreturn
+  [x]
+  (println x)
+  x)
